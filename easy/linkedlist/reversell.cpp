8120e2c2e81657632149e977d4bab69cd9fb9f59
Abhishek Kakolla <abhishekkakolla1@gmail.com>
2024-11-14 23:19:48 -0800

    LL

--- easy/linkedlist/reversell.cpp
+++ easy/linkedlist/reversell.cpp
@@ -2,28 +2,30 @@
 Key concept: keep track of the nextnode pointer so you don't lose the access to the rest of the list
 */
 
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+// Definition for singly-linked list.
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
 
-class Solution {
+class Solution
+{
 public:
-    ListNode* reverseList(ListNode* head) {
-        if (head == nullptr) return nullptr;
-        ListNode* prev = head;
-        ListNode* curr = head->next;
+    ListNode *reverseList(ListNode *head)
+    {
+        if (head == nullptr)
+            return nullptr;
+        ListNode *prev = head;
+        ListNode *curr = head->next;
         prev->next = nullptr;
 
         while (curr != nullptr)
         {
-            ListNode* nextnode = curr->next;
+            ListNode *nextnode = curr->next;
             curr->next = prev;
             prev = curr;
             curr = nextnode;
